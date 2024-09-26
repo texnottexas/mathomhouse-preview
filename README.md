5117fbb645f3f0cf954c0a3d5c54d8fb7cfdbb0e
kimho2 <kimho2@gmail.com>
2024-09-25 22:11:13 -0400

    First calculator
    Additional pages
    Frame for VIT calculator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TW-Calculators
+
+Calculators for various aspects of the mobile game Top War.
+
+VIT
+Equipment
+Components
+Gems
+RSS
+Transfer Tickets
+XP Books
+Exclusive Skills
---
+++ vit.js
@@ -0,0 +1,25 @@
+function calculateVit() {
+    // Retrieve input values
+    const largeCaps = parseFloat(document.getElementById('vitLargeCapsule').value) || 0;
+    const smallCaps = parseFloat(document.getElementById('vitSmallCapsule').value) || 0;
+    const plus50s = parseFloat(document.getElementById('vitPlus50').value) || 0;
+    const plus10s = parseFloat(document.getElementById('vitPlus10').value) || 0;
+    const flowers = parseFloat(document.getElementById('vitFlower').value) || 0;
+    const hearts = parseFloat(document.getElementById('vitHeart').value) || 0;
+
+    //VIT values
+    var largeCapValue = 50;
+    var smallCapValue = 10;
+    var plus50Value = 50;
+    var plus10Value = 10;
+    var flowerValue = 30;
+    var heartValue = 30;
+
+
+    const result = (largeCaps * largeCapValue) + (smallCaps * smallCapValue) +
+        (plus50s * plus50Value) + (plus10s * plus10Value) +
+        (flowers * flowerValue) + (hearts * heartValue);
+
+    // Display the result
+    document.getElementById('result').innerHTML = `Your total VIT is: ${result}`;
+}

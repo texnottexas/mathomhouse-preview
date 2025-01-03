c6b998e56603cd47438ecd1bf63a769def4b8286
kimho2 <kimho2@gmail.com>
2025-01-02 20:54:00 -0500

    Cleanup and tweaks
    Code cleanup and some tweaks.

--- EnigmaDominators.js
+++ EnigmaDominators.js
@@ -1,3 +1,5 @@
+import {weekMapData, colors} from './EnigmaDominatorsWeekly.js';
+
 document.addEventListener("DOMContentLoaded", loadMap);
 const defaultSid = 1668;
 let mapData = [];
@@ -7,42 +9,6 @@ let centeredSid = 1668;
 let highlightedSids = '';
 let serverNumbers = [];
 
-const weekMapData = [
-    {
-        round: 'Practice3',
-        //file: './EnigmaDominators/ED_Prep_FactionDataRound3.csv'
-        file: 'https://mathomhouse.github.io/EnigmaDominators/ED_Prep_FactionDataRound3.csv'
-    },
-    {
-        round: 'Practice4',
-        //file: './EnigmaDominators/ED_Prep_FactionDataRound4.csv'
-        file: 'https://mathomhouse.github.io/EnigmaDominators/ED_Prep_FactionDataRound4.csv'
-    },
-]
-
-const colors = [
-    '#FFC0CB', // Bright Pink
-    '#FFD700', // Gold
-    '#FFA07A', // Light Salmon
-    '#FF7F50', // Coral
-    '#FF6347', // Tomato
-    '#FFDAB9', // Peach Puff
-    '#FFB6C1', // Light Pink
-    '#FFE4B5', // Moccasin
-    '#FFFF99', // Light Yellow
-    '#B0E57C', // Light Green
-    '#98FB98', // Pale Green
-    '#87CEFA', // Light Sky Blue
-    '#87CEEB', // Sky Blue
-    '#ADD8E6', // Light Blue
-    '#00BFFF', // Deep Sky Blue
-    '#F0E68C', // Khaki
-    '#DDA0DD', // Plum (Purple)
-    '#DA70D6', // Orchid (Purple)
-    '#BA55D3', // Medium Orchid (Purple)
-    '#9370DB', // Medium Purple
-    '#E6E6FA'  // Lavender
-];
 
 // Load the JSON map data
 function loadMap(){
---
+++ EnigmaDominatorsWeekly.js
@@ -0,0 +1,39 @@
+
+export const weekMapData = [
+    {
+        round: 'Practice3',
+        file: './EnigmaDominators/ED_Prep_FactionDataRound3.csv'
+        //file: 'https://mathomhouse.github.io/EnigmaDominators/ED_Prep_FactionDataRound3.csv'
+    },
+    {
+        round: 'Practice4',
+        file: './EnigmaDominators/ED_Prep_FactionDataRound4.csv'
+        //file: 'https://mathomhouse.github.io/EnigmaDominators/ED_Prep_FactionDataRound4.csv'
+    },
+]
+
+export const colors = [
+    '#FFC0CB', // Bright Pink
+    '#FFD700', // Gold
+    '#FFA07A', // Light Salmon
+    '#FF7F50', // Coral
+    '#FF6347', // Tomato
+    '#FFDAB9', // Peach Puff
+    '#FFB6C1', // Light Pink
+    '#FFE4B5', // Moccasin
+    '#FFFF99', // Light Yellow
+    '#B0E57C', // Light Green
+    '#98FB98', // Pale Green
+    '#87CEFA', // Light Sky Blue
+    '#87CEEB', // Sky Blue
+    '#ADD8E6', // Light Blue
+    '#00BFFF', // Deep Sky Blue
+    '#F0E68C', // Khaki
+    '#DDA0DD', // Plum (Purple)
+    '#DA70D6', // Orchid (Purple)
+    '#BA55D3', // Medium Orchid (Purple)
+    '#9370DB', // Medium Purple
+    '#E6E6FA'  // Lavender
+];
+
+//export {weekMapData, colors}

be4a475b7458b82374e54f9ad210c58503ff3940
kimho2 <kimho2@gmail.com>
2025-01-09 20:02:58 -0500

    Adding colors

--- EnigmaDominatorsWeekly.js
+++ EnigmaDominatorsWeekly.js
@@ -12,7 +12,6 @@ export const colors = [
     '#FFD700', // Gold
     '#98FB98', // Pale Green
     '#FFA07A', // Light Salmon
-    '#FF7F50', // Coral
     '#FF6347', // Tomato
     '#FFDAB9', // Peach Puff
     '#FFB6C1', // Light Pink
@@ -21,12 +20,32 @@ export const colors = [
     '#B0E57C', // Light Green
     '#87CEFA', // Light Sky Blue
     '#87CEEB', // Sky Blue
-    '#ADD8E6', // Light Blue
     '#00BFFF', // Deep Sky Blue
     '#F0E68C', // Khaki
     '#DDA0DD', // Plum (Purple)
     '#DA70D6', // Orchid (Purple)
     '#BA55D3', // Medium Orchid (Purple)
     '#9370DB', // Medium Purple
-    '#E6E6FA'  // Lavender
+    '#E6E6FA',  // Lavender
+    '#3CB371', // Medium Sea Green
+    '#FF8C69', // Salmon Pink
+    '#FFA500', // Medium Orange
+    '#F4A460', // Sandy Brown
+    '#DDA0DD', // Medium Plum
+    '#FF69B4', // Hot Pink
+    '#FF6347', // Medium Tomato
+    '#FFD700', // Goldenrod Yellow
+    '#98FB98', // Soft Lime Green
+    '#1E90FF', // Dodger Blue
+    '#6495ED', // Cornflower Blue
+    '#87CEFA', // Medium Light Sky Blue
+    '#00CED1', // Medium Turquoise
+    '#7FFFD4', // Aquamarine
+    '#BA55D3', // Orchid
+    '#DA70D6', // Medium Orchid
+    '#9370DB', // Purple Medium
+    '#FFB347', // Pastel Orange
+    '#FFB6C1', // Pastel Pink
+    '#E9967A', // Dark Salmon
+    '#FFA07A', // Coral Medium
 ];
